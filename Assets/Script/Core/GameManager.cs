405d639e0ad510b0c9816486548727e1adb2038a
kflanagan <kenneth.of.borg@gmail.com>
2024-11-11 14:36:18 -0700

    minor changes

--- Assets/Script/Core/GameManager.cs
+++ Assets/Script/Core/GameManager.cs
@@ -342,7 +342,6 @@ namespace Assets.Core
         public GameController GameController;
         public bool _weAreFriend = false;
         public bool _warpingInIsOver = false; // WarpingInCompleted() called from E_Animator3 sets true and set false again in CombatCompleted state in BeginState
-
         //public Galaxy Galaxy; // Was part of SolarSystemView - Galaxy.cs
         private SolarSystemView solarSystemView;
         /// <summary>
