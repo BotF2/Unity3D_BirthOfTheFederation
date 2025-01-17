3bc355cd8dcb1792e74ef2c3cf9b3653b4d6a9e6
Kenneth Flanagan <kenneth.of.borg@gmail.com>
2025-01-17 10:01:21 -0700

    Basic Theme SO system in place for the Galaxy Menu. Moving on to MenuManager.

--- Assets/Script/Core/ThemeSO.cs
+++ Assets/Script/Core/ThemeSO.cs
@@ -11,6 +11,8 @@ public class ThemeSO: ScriptableObject
     public Sprite BackImage;
     public Sprite Insignia;
     public Sprite RaceImage;
+    public Sprite SystemImage;
+    public Sprite FleetShipImage;
     public Color BackgroundColor;
     public Color ForegroundColor;
     public Color BoarderColor;
